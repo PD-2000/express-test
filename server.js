986186c5d0614abc182a3ae7a64c8bcd10c7146c
Pawel Domagala <domagalapawel11@gmail.com>
2023-06-11 01:17:14 +0200

    Init files

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use((req, res, next) => {
+  const forbiddenPath = '/user/';
+  if(req.path.startsWith(forbiddenPath))
+    return res.sendFile(path.join(__dirname, '/views/forbidden.html'));
+  next();
+});
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+  res.show('home.html');
+});
+
+app.get('/home', (req, res) => {
+  res.show('home.html');
+});
+
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '/public/404.png'));
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
