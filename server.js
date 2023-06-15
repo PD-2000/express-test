699d52a9d1bfb4716eecc6a811a144bf6b924878
Pawel Domagala <domagalapawel11@gmail.com>
2023-06-15 20:10:38 +0200

    Update forbidden functionality

--- server.js
+++ server.js
@@ -2,30 +2,23 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use((req, res, next) => {
-  const forbiddenPath = '/user/';
-  if(req.path.startsWith(forbiddenPath))
-    return res.sendFile(path.join(__dirname, '/views/forbidden.html'));
-  next();
+app.use('user', (req, res, next) => {
+  return res.sendFile(path.join(__dirname, '/views/forbidden.html'));
 });
-
 app.use((req, res, next) => {
   res.show = (name) => {
     res.sendFile(path.join(__dirname, `/views/${name}`));
   };
   next();
 });
-
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
   res.show('home.html');
 });
-
 app.get('/home', (req, res) => {
   res.show('home.html');
 });
-
 app.get('/about', (req, res) => {
   res.show('about.html');
 });
